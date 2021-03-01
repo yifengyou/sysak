760a78e93d9fbabaf74be5b81ca62ce68ca8b2f2
Wetp Zhang <weipu.zy@alibaba-inc.com>
2021-03-01 10:58:33 +0800

    remove unused module
    Signed-off-by: Wetp Zhang <weipu.zy@alibaba-inc.com>

--- source/lib/internal/kernel_module/sysak_mods.c
+++ source/lib/internal/kernel_module/sysak_mods.c
@@ -4,7 +4,7 @@ extern int test_init(void);
 extern int test_exit(void);
 
 struct sysak_module sysak_modules[] = {
-	{ "test_module", test_init, test_exit},
+/*	{ "test_module", test_init, test_exit},*/ //this is a example
 };
 
 const int sysk_module_num = sizeof(sysak_modules) / sizeof(struct sysak_module);
