a37b5fedd337f0a698d8e013022c43c1754a1c90
Wetp Zhang <weipu.zy@alibaba-inc.com>
2021-03-02 10:12:09 +0800

    add trace_sig tool
    Signed-off-by: Wetp Zhang <weipu.zy@alibaba-inc.com>

--- source/lib/internal/kernel_module/sysak_mods.c
+++ source/lib/internal/kernel_module/sysak_mods.c
@@ -2,9 +2,12 @@
 
 extern int test_init(void);
 extern int test_exit(void);
+extern int trace_sig_init(void);
+extern int trace_sig_exit(void);
 
 struct sysak_module sysak_modules[] = {
 /*	{ "test_module", test_init, test_exit},*/ //this is a example
+	{ "trace_sig", trace_sig_init, trace_sig_exit},
 };
 
 const int sysk_module_num = sizeof(sysak_modules) / sizeof(struct sysak_module);
