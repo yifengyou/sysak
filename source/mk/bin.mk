d87cd9cf667e7cfc666a5bb021cc118fde3264c6
Wetp Zhang <weipu.zy@alibaba-inc.com>
2021-04-05 14:14:25 +0800

    add the third-part binary support
    Signed-off-by: Wetp Zhang <weipu.zy@alibaba-inc.com>

---
+++ source/mk/bin.mk
@@ -0,0 +1,19 @@
+ifeq ($(KERNEL_DEPEND), Y)
+TARGET_PATH := $(OBJ_TOOLS_PATH)
+SOURCE_PATH := $(KERNEL_VERSION)
+else
+TARGET_PATH := $(OBJ_TOOLS_ROOT)
+SOURCE_PATH := .
+endif
+
+exist := $(shell if [ -f $(SOURCE_PATH)/$(target) ]; then echo "exist"; else echo "notexist"; fi;)
+ifeq ($(exist), exist)
+$(target):
+	cp $(SOURCE_PATH)/$(target) $(TARGET_PATH)/
+	echo $(target):$(DEPEND) >> $(OBJ_TOOLS_PATH)/$(SYSAK_RULES)
+else
+$(target):
+	@echo no kernel version
+endif
+
+.PHONY: $(target)
