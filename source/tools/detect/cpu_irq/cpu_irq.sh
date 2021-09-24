75ce610b5d4aad73e3509068b9804c865862aa8f
Wetp Zhang <weipu.zy@alibaba-inc.com>
2021-09-24 23:12:10 +0800

    cpuirq: add top_irq function
    Signed-off-by: Wetp Zhang <weipu.zy@alibaba-inc.com>

--- source/tools/detect/cpu_irq/cpu_irq.sh
+++ source/tools/detect/cpu_irq/cpu_irq.sh
@@ -7,19 +7,29 @@
 # Modify Date: 2021-02-09 15:21
 # Function: 
 #***************************************************************#
+
+usage() {
+	echo "sysak cpuirq: show irq information"
+        echo "options: -h, help information"
+        echo "         -c cpu, specify the cpu"
+        echo "         -i interval, the interval checking the irq status"
+        echo "         -b, show irq bind on cpu"
+        echo "         -t, show top irq during the interval time"
+}
+
+irq_bind() {
 maxcpu=`lscpu | grep "On-line" | awk -F- '{print $3}'`
-if [ "$1" -ge 0 ] 2>/dev/null; then
-	if [ $1 -gt $maxcpu ]; then
+if [ "$cpu" -ge 0 ] 2>/dev/null; then
+	if [ $cpu -gt $maxcpu ]; then
 		echo cpu is not valid
 		exit
 	fi
 else
-	echo Usage:
-	echo "     sysak cpu_irq cpu"
+	usage
 	exit
 fi
 
-echo show cpu $1
+echo show irq bind on cpu $cpu
 OLD_IFS="$IFS"
 for irq in `ls /proc/irq`; do
 	if [ "$irq" -ge 0 ] 2>/dev/null; then
@@ -31,10 +41,10 @@ for irq in `ls /proc/irq`; do
 			range=($item)			
 			min=${range[0]}
 			max=${range[1]}
-			if [ ${range[0]} -eq $1 ]; then
+			if [ ${range[0]} -eq $cpu ]; then
 				echo $irq
 			else
-				if [[ ${range[0]} -lt $1 ]] && [[ ${range[1]} -ge $1 ]]; then
+				if [[ ${range[0]} -lt $cpu ]] && [[ ${range[1]} -ge $cpu ]]; then
 					echo $irq
 				fi
 			fi
@@ -42,3 +52,61 @@ for irq in `ls /proc/irq`; do
 	fi
 done
 IFS="$OLD_IFS"
+}
+
+datadir="/var/log/sysak"
+interval=5
+show_top="false"
+show_bind="false"
+
+top_irq() {
+echo > $datadir/cpuirq1.log
+echo > $datadir/cpuirq2.log
+cat /proc/interrupts | while read line; do echo $line |awk '{for(i=2;i<=NF-2;i++) sum+=$i} END{print $1sum":"$NF}' >> $datadir/cpuirq1.log;done
+sleep $interval
+cat /proc/interrupts | while read line; do echo $line |awk '{for(i=2;i<=NF-2;i++) sum+=$i} END{print $1sum":"$NF}' >> $datadir/cpuirq2.log;done
+diff -y --suppress-common-line -B /var/log/sysak/cpuirq1.log /var/log/sysak/cpuirq2.log | awk -F ":" '{print $4-$2" "$1":"$5}' |  sort  -nr > $datadir/cpuirq.log
+
+echo the top interrups at last $interval seconds:
+cat $datadir/cpuirq.log | while read line; do
+	irqcnt=`echo $line | awk '{print $1}'`
+	if [ $irqcnt -lt 1000 ]; then
+		exit;
+	fi
+	echo $line
+done
+}
+
+
+while getopts 'i:c:bth' OPT; do
+        case $OPT in
+                "h")
+                        usage
+                        exit 0
+                        ;;
+                "c")
+                        cpu=$OPTARG
+                        ;;
+                "i")
+                        interval="$OPTARG"
+                        ;;
+                "t")
+                        show_top="true"
+                        ;;
+                "b")
+                        show_bind="true"
+                        ;;
+                *)
+                        usage
+                        exit -1
+                ;;
+        esac
+done
+
+if [ $show_top == "true" ];then
+	top_irq
+fi
+
+if [ $show_bind == "true" ];then
+	irq_bind
+fi
