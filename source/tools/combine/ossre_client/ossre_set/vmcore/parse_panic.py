f6688dbc27bca0f712d14b0108a734e12e35b425
Liangyan <liangyan.peng@linux.alibaba.com>
2021-10-23 12:48:44 +0800

    ossre: use sqlite db for solution info

--- source/tools/combine/ossre_client/ossre_set/vmcore/parse_panic.py
+++ source/tools/combine/ossre_client/ossre_set/vmcore/parse_panic.py
@@ -39,22 +39,6 @@ title_pattern = re.compile(r'\[\s*\S+\] ((BUG:|Kernel panic|Bad pagetable:|divid
 vertype_pattern = re.compile(r'(\d+)\.(\d+)\.')
 last_strhost = ''
 
-calltrace_table = {
-"kmem_cache_close$__kmem_cache_shutdown$kmem_cache_destroy":{"310":"b8529907ba35d625fa4b85d3e4dc8021be97c1f3"},
-"inode_permission$link_path_walk":{"310":"4023bfc9f351a7994fb6a7d515476c320f94a574"},
-"__idr_remove_all$idr_destroy$ida_destroy":{"310":"40b320e1c757353d65550471132683dfb6c35e0f"},
-"_raw_spin_lock$__page_check_address$try_to_unmap_one":{"310":"98398c32f6687ee1e1f3ae084effb4b75adb0747"},
-"free_percpu$percpu_counter_destroy$wb_init":{"310":"078c6c3a5e7dc53a9a23408cc32c83954abb5d0d"},
-}
-
-bugon_table = {
-"arch/x86/mm/hugetlbpage.c":{"310":{"calltrace":"huge_pte_alloc$hugetlb_fault","commitid":"0d777df5d8953293be090d9ab5a355db893e8357+4e666314d286765a9e61818b488c7372326654ec"}},
-"block/blk-core.c":{"310":{"calltrace":"__blk_put_request$blk_put_request$sg_finish_rem_req","commitid":"f3951a3709ff50990bf3e188c27d346792103432"}},
-}
-
-hardlockup_table = {
-}
-
 def get_column_value(column, line):
     match = rip_pattern.match(line)
     if match:
@@ -196,7 +180,7 @@ def fixup_panic(column):
     finally:
         return result
 
-def check_panic(column):
+def check_panic(column,conn):
     if 'rawdmesg' not in column and os.path.isfile(column['filename']) == False:
         return
 
@@ -222,26 +206,46 @@ def check_panic(column):
     elif column['crashkey_type'] == 3 and len(column['bugon_file']) > 0:
         column['crashkey'] = '%d$%s$%s'%(column['vertype'],column['bugon_file'],column['calltrace'])
 
-    for key in calltrace_table:
-        if key in column['calltrace']:
-            if (column['vertype'] > 0 and str(column['vertype']) in calltrace_table[key]):
-                column['commitid'] = calltrace_table[key][str(column['vertype'])]
-            if column['vertype'] == 0:
-                commitinfos = calltrace_table[key]
-                column['solution'] = ("疑似upstream fix:%s"(commitinfos))
+    cursor = conn.cursor()
+    issue_id = ''
+    if len(column['bugon_file']) > 0:
+        sql = 'select issue_id,vertype from panic where bugon_file=? and instr(?,calltrace)!=0'
+        value = (column['bugon_file'],column['calltrace'],)
+        cursor.execute(sql, value)
+        rows = cursor.fetchall()
+        if len(rows) <= 0:
+            sql = 'select issue_id,vertype from panic where bugon_file=? and instr(calltrace,?)!=0'
+            cursor.execute(sql, value)
+            rows = cursor.fetchall()
+        for row in rows:
+            if row[1] == column['vertype'] or len(issue_id) <= 0:
+                issue_id = row[0]
+    else:
+        sql = 'select issue_id,vertype from panic where instr(?,calltrace)!=0'
+        value = (column['calltrace'],)
+        cursor.execute(sql, value)
+        rows = cursor.fetchall()
+        if len(rows) <= 0:
+            sql = 'select issue_id,vertype from panic where instr(?,calltrace)!=0'
+            cursor.execute(sql, value)
+            rows = cursor.fetchall()
+        for row in rows:
+            if row[1] == column['vertype'] or len(issue_id) <= 0:
+                issue_id = row[0]
+ 
+    sql = 'select commitid,solution from issue where issue_id=?'
+    value = (issue_id,)
+    cursor.execute(sql, value)
+    row = cursor.fetchone()
+    if row:
+        if len(row[0]) > 0 and len(row[1]) > 0:
+            column['commitid'] = row[0]
+            column['solution'] = row[1]
+        elif len(row[0]) > 0:
+            column['commitid'] = row[0]
+            column['solution'] = ("疑似upstream fix:%s"%(row[0]))
 
-    if len(column['bugon_file']) > 0 and column['bugon_file'] in bugon_table:
-        if( column['vertype'] > 0 and str(column['vertype']) in bugon_table[column['bugon_file']]):
-            commitinfo = bugon_table[column['bugon_file']][str(column['vertype'])]
-            if (len(commitinfo['calltrace']) > 0 and
-                commitinfo['calltrace'] in column['calltrace']): 
-                column['commitid'] = commitinfo['commitid']
-        if column['vertype'] == 0:
-            commitinfos = bugon_table[column['bugon_file']]
-            for ver in commitinfos:
-                if (len(commitinfos[ver]["calltrace"]) > 0 and
-                    commitinfos[ver]["calltrace"] in column['calltrace']):
-                    column['solution'] = ("疑似upstream fix:%s"(commitinfos[ver]["commitid"]))
+    cursor.close()
     fixup_panic(column)
 
     if (len(column['commitid']) > 0 or len(column['solution']) > 0):
@@ -307,6 +311,10 @@ def query(sn, data, log_file="", crashonly=0):
     column = {}
 
     try:
+        conn = sqlite3.connect('%s/vmcore_sqlite.db'%(os.path.dirname(os.path.abspath(__file__))))
+        conn.text_factory = str
+        #cursor = conn.cursor()
+
         if crashonly == 1:
             result = {}
             vmcore_file = data['vmcore']
@@ -315,7 +323,7 @@ def query(sn, data, log_file="", crashonly=0):
             dmesgs = crash_inst.cmd("log")
             init_column(column)
             column['rawdmesg'] = dmesgs
-            matched = check_panic(column)
+            matched = check_panic(column,conn)
             result['vmcore_file'] = vmcore_file
             if matched:
                 ret['return'] = True
@@ -341,7 +349,7 @@ def query(sn, data, log_file="", crashonly=0):
             column['filename'] = log_file
             column['ver'] = ''
             result['dmesg_file'] = log_file
-            matched = check_panic(column)
+            matched = check_panic(column,conn)
             if matched:
                 ret['return'] = True
                 if len(column['commitid']) > 0:
@@ -370,7 +378,7 @@ def query(sn, data, log_file="", crashonly=0):
                         column['filename'] = filepath 
                         column['ver'] = ''
                         result['dmesg_file'] = filepath
-                        matched = check_panic(column)
+                        matched = check_panic(column,conn)
                         if matched:
                             ret['return'] = True
                             if len(column['commitid']) > 0:
@@ -393,6 +401,8 @@ def query(sn, data, log_file="", crashonly=0):
         traceback.print_exc()
     finally:
         column['dmesg'] = ""
+        if (conn):
+            conn.close()
         #print(column)
         print ('VMCORE:%s' %( json.dumps(ret, ensure_ascii=False)))
         return ret
