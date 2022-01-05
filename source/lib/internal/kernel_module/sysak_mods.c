0650baa2de03ae51d9ecf643d96a991cf4ec433c
Hailong Liu <liuhailong@linux.alibaba.com>
2022-01-05 09:55:05 +0800

    monitor/sched: Add schedule trace function
    Schedule trace function used to trace a thread's schedule events.
    It provide a easy way for users to get schedule infomations, and
    easy way to control.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/lib/internal/kernel_module/sysak_mods.c
+++ source/lib/internal/kernel_module/sysak_mods.c
@@ -40,11 +40,22 @@ int __attribute__((weak)) task_ctl_exit(void)
 	return 0;
 }
 
+int __attribute__((weak)) schedtrace_init(void)
+{
+	return 0;
+}
+
+int __attribute__((weak)) schedtrace_exit(void)
+{
+	return 0;
+}
+
 struct sysak_module sysak_modules[] = {
 	{ "trace_sig", trace_sig_init, trace_sig_exit},
 	{ "memleak", memleak_init, memleak_uninit},
 	{ "trace_irqoff", trace_irqoff_init, trace_irqoff_exit},
 	{ "task_ctl", task_ctl_init, task_ctl_exit},
+	{ "schedtrace", schedtrace_init, schedtrace_exit},
 };
 
 const int sysk_module_num = sizeof(sysak_modules) / sizeof(struct sysak_module);
