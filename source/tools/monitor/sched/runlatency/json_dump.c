563d82ead4ec6a5efc7e47515b761c957cc493ba
Hailong Liu <liuhailong@linux.alibaba.com>
2022-02-24 11:58:39 +0800

    runlatency: Refactoring the code and directory structure
    Combine the json_dump and sh into one directory.
    
    Signed-off-by: Hailong Liu <liuhailong@linux.alibaba.com>

--- source/tools/monitor/sched/runlatency/json_dump.c
+++ source/tools/monitor/sched/runlatency/json_dump.c
@@ -5,11 +5,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 #include "parser.h"
 
-#define IRQOFF_FILE  "/proc/runlatency/irqoff/latency"
-#define NOSCH_FILE   "/proc/runlatency/nosch/stack_trace"
-#define RUNQ_FILE	 "/proc/runlatency/runqlat/runqlat"
+#define IRQOFF_FILE  "/proc/sysak/runlatency/irqoff/latency"
+#define NOSCH_FILE   "/proc/sysak/runlatency/nosch/stack_trace"
+#define RUNQ_FILE	 "/proc/sysak/runlatency/runqlat/runqlat"
 
 #define STREAM_SIZE	(128 * 1024)
 
@@ -19,6 +20,8 @@ int read_file(char* path, char* s)
 	int size;
 	
 	if (fd < 0) {
+		fprintf(stderr, "%s :open %s\n",
+			strerror(errno), path);
 		return fd;
 	}
 	
@@ -33,6 +36,8 @@ int clear_file(char *path)
 	int size;
 	
 	if (fd < 0) {
+		fprintf(stderr, "%s :open %s\n",
+			strerror(errno), path);
 		return fd;
 	}
 	
@@ -41,29 +46,35 @@ int clear_file(char *path)
 	return size;
 }
 
-int main(void)
+int pasre_dump(char *file)
 {
 	char *s;
 	int ret;
+	FILE *outf = NULL;
 	
 	s = malloc(STREAM_SIZE);
 	if (s == NULL) {
 		return -ENOMEM;
 	}
-	
+	if (file) {
+		outf = fopen(file, "a+");
+		if (!outf)
+			fprintf(stderr, "%s :fopen %s\n",
+				strerror(errno), file);
+	}
 	ret = read_file(IRQOFF_FILE, s);
 	if (ret < 0) {
 		goto failed;
 	}
 	s[ret] = '\0';
-	parser_irqoff(s, ret);
+	parser_irqoff(s, ret, outf);
 	
 	ret = read_file(NOSCH_FILE, s);
 	if (ret < 0) {
 		goto failed;
 	}
 	s[ret] = '\0';
-	parser_nosch(s, ret);
+	parser_nosch(s, ret, outf);
 	clear_file(NOSCH_FILE);
 	
 	ret = read_file(RUNQ_FILE, s);
@@ -71,13 +82,17 @@ int main(void)
 		goto failed;
 	}
 	s[ret] = '\0';
-	parser_runq(s, ret);
+	parser_runq(s, ret, outf);
 	clear_file(RUNQ_FILE);
 	
 	free(s);
+	if (outf)
+		fclose(outf);
 	return 0;
 	
 failed:
 	free(s);
+	if (outf)
+		fclose(outf);
 	return ret;
 }
