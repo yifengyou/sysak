fdb5b4d66612241f8c83aea5433614efc86691a2
chengshuyi.csy <chengshuyi@linux.alibaba.com>
2022-03-10 16:40:09 +0800

    btf: support to generate btf file online.
    At present, only some kernel versions of btf files are provided, but
    there are many self-developed kernel versions adopted by users, so it is
    necessary to provide the ability to generate btf files online.
    
    Signed-off-by: chengshuyi.csy <chengshuyi@linux.alibaba.com>

--- source/tools/combine/btf/btf.sh
+++ source/tools/combine/btf/btf.sh
@@ -5,12 +5,14 @@
 zip_path=${SYSAK_WORK_PATH}/tools/BTF/btf.7z
 btf_dir=${SYSAK_WORK_PATH}/tools
 btf_path=${btf_dir}/vmlinux-$(uname -r)
+vmlinux_path=""
 
 usage() {
     echo "sysak btf: Extract the btf file to the specified directory"
     echo "options: -h, help information"
     echo "         -d, Specify the path, default directory path: ${btf_dir}"
     echo "         -l, show vmlinux btf list"
+    echo "         -g, Specify the vmlinux file path, which is used to generate btf. The generated btf file path is: ${btf_path} "
 }
 
 extract_btf() {
@@ -22,11 +24,19 @@ extract_btf() {
     cp $source_path ${btf_path}
 }
 
+generate_btf() {
+    if [ ! -f "$vmlinux_path" ]; then
+        echo "vmlinux file not exist: $vmlinux_path}"
+    fi
+    ${btf_dir}/pahole -J --kabi_prefix=__UNIQUE_ID_rh_kabi_hide --btf_encode_detached=${btf_path} ${vmlinux_path}
+    echo "btf file has been generated, the path is: ${btf_path}"
+}
+
 show_list() {
     ls ${SYSAK_WORK_PATH}/tools/vmlinux-btf
 }
 
-while getopts 'd:lh' OPT; do
+while getopts 'g:d:lh' OPT; do
     case $OPT in
     "h")
         usage
@@ -35,11 +45,14 @@ while getopts 'd:lh' OPT; do
     "d")
         btf_dir=$OPTARG
         btf_path=${btf_dir}/vmlinux-$(uname -r)
-
         ;;
     "l")
         show_list
         exit 0
+        ;;
+    "g")
+        vmlinux_path=$OPTARG
+        generate_btf
         ;;
     *)
         usage
